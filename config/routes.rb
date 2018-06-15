432df5a7bf42f53e85b0dd2df62aaa9d4031fb40
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2018-06-14 20:29:15 -0500

    made our own route on portfolis fetures

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  
+  #get portfolio routes to change portfolios/:id to portoflio/:id in the show action. Rails knows when that when :id is a get route it is looking for params to pass to the show action
+  resources :portfolios, expect: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
